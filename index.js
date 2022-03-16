e99480cff1cde4fc7cfdfd5dfa65d6d0b44c0215
Paschal Ramdan <dane.ramdan@gmail.com>
2022-03-16 13:44:32 -0400

    commit tree 1c41d13283b77a049498e8096279195c403d8326

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = 'james'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john'
+  }
+
+
+
+
+
+  
+//     describe('changeLeastFavoriteCustomer()', function() {
+//       it('unsuccessfully tries to reassign the least favorite customer', function() {
+//         expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+//       });
+//     });
+//   });
+  
